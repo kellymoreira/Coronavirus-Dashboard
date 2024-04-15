772e3379a6c2598be9cb80ecb9dcd23f22dca7fa
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2024-04-15 19:47:50 -0300

    adicionando comentários

--- database/config.php
+++ database/config.php
@@ -1,10 +1,13 @@
 <?php
 
+    // Inclui o arquivo autoload.php do Composer para carregar as dependências
     require './vendor/autoload.php'; 
     
+    // Cria uma instância do Dotenv para carregar as variáveis de ambiente
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
     $dotenv->load();
-  
+    
+    // Obtém as variáveis de ambiente(.env) para a conexão com o banco de dados
     $host = $_ENV['DB_HOST'];
     $dbname = $_ENV['DB_NAME']; 
     $user = $_ENV['DB_USER'];
