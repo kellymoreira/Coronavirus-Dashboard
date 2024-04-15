0a560231d411b10edc41228ffa86f94db02c8dfb
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2024-04-15 19:35:20 -0300

    organizando o código e adicionando comentários

--- database/insert.php
+++ database/insert.php
@@ -5,12 +5,16 @@
  * ao banco de dados 'covid_19' na tabela 'access_records' do MySQL via phpMyAdmin.
  ***************************************************************************************************/
 
+// Inclui o arquivo de configuração que contém informações de conexão com o banco de dados
 require 'config.php';
 
+// Conecta ao banco de dados MySQL utilizando as informações do arquivo de configuração
 $connection = mysqli_connect($host, $user, $password, $dbname);
 
+// Verifica se a conexão foi estabelecida com sucesso
 if (!$connection) {
-    die("Conexão falhou: " . mysqli_connect_error());
+  // Se a conexão falhar, encerra o script e exibe uma mensagem de erro
+  die("Conexão falhou: " . mysqli_connect_error());
 }
 
 // Configura o fuso horário e obtém a data e hora atual
@@ -29,10 +33,10 @@ $sql = "INSERT INTO access_records (
 
 // Executa a consulta SQL e trata os erros
 if (mysqli_query($connection, $sql)) {
-    // Se a consulta for bem-sucedida, não é necessário fazer nada aqui
+  // Se a consulta for bem-sucedida, não é necessário fazer nada aqui
 } else {
-    // Se houver um erro na consulta, imprime a mensagem de erro
-    echo "Erro: " . mysqli_error($connection);
+  // Se houver um erro na consulta, imprime a mensagem de erro
+  echo "Erro: " . mysqli_error($connection);
 }
 
 // Fecha a conexão com o banco de dados
