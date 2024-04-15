5777301ba39dee617a1ee7a18e074c9f7f18b4d9
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2024-04-15 19:05:31 -0300

    organizando comentários e código

--- database/insert.php
+++ database/insert.php
@@ -1,37 +1,41 @@
 <?php
-/*********************************************************************************************************
- * esse arquivo PHP insere os dados de último acesso (gráfico de um dos países escolhidos por estado) 
- * e insere ao banco de dados 'covid_19' na tabela 'access_records' do MySQL via phpMyAdmin.
- ********************************************************************************************************/
-  require 'config.php';
 
-  //$conn = mysqli_connect($host, $user, $password, $dbname);
+/***************************************************************************************************
+ * Insere os dados de último acesso ao gráfico de um dos países escolhidos que mostra por estados
+ * ao banco de dados 'covid_19' na tabela 'access_records' do MySQL via phpMyAdmin.
+ ***************************************************************************************************/
 
-  $connection = mysqli_connect($host, $user, $password, $dbname);
+require 'config.php';
 
+$connection = mysqli_connect($host, $user, $password, $dbname);
 
-  //if (!$conn) {
-    if (!$connection) {
+if (!$connection) {
     die("Conexão falhou: " . mysqli_connect_error());
-  }
+}
 
-  $timeZone = new DateTimeZone('America/Sao_Paulo');
-  $dateTime = (new DateTime('now', $timeZone))->format('Y-m-d H:i:s');
+// Configura o fuso horário e obtém a data e hora atual
+$timeZone = new DateTimeZone('America/Sao_Paulo');
+$dateTime = (new DateTime('now', $timeZone))->format('Y-m-d H:i:s');
 
+// Prepara a consulta SQL
+$sql = "INSERT INTO access_records (
+            access_date_time,
+            country
+        ) 
+        VALUES (
+            '$dateTime',
+            '$country'
+        )";
 
-  $sql = "INSERT INTO access_records (access_date_time, country) VALUES ('$dateTime', '$country')";
-
-  //if (mysqli_query($conn, $sql)) {
-  if (mysqli_query($connection, $sql)) {
-
-    //echo "Dados inseridos com sucesso!";
-  } else {
-    //echo "Erro: " . mysqli_error($conn);
+// Executa a consulta SQL e trata os erros
+if (mysqli_query($connection, $sql)) {
+    // Se a consulta for bem-sucedida, não é necessário fazer nada aqui
+} else {
+    // Se houver um erro na consulta, imprime a mensagem de erro
     echo "Erro: " . mysqli_error($connection);
-  }
+}
 
-  //mysqli_close($conn);
-  mysqli_close($connection);
-
+// Fecha a conexão com o banco de dados
+mysqli_close($connection);
 
 ?>
