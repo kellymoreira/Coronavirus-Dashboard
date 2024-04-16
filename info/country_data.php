5480825eb6c2e24ca0588ecf84a71ca77f934a01
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2024-04-15 21:54:32 -0300

    adicionando comentários e ajustando syntaxe

--- info/country_data.php
+++ info/country_data.php
@@ -12,14 +12,22 @@
 
   <?php
 
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  /*******************************************************************************************************
+   * Verifica se os dados foram enviados via método POST. Se sim, obtém o país selecionado do formulário,
+   * caso contrário, define o país padrão como Brasil.
+   *******************************************************************************************************/
+
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $country = $_POST['country'];
   } else {
     $country = 'Brazil';
   }
 
+  // URL da API para obter os dados do COVID-19
   $url = 'https://dev.kidopilabs.com.br/exercicio/covid.php?pais=';
   $completeUrl = $url . $country;
+
+  // Obtém os dados JSON da API
   $json = file_get_contents($completeUrl);
   $data = json_decode($json);
   $numberKeys = count(get_object_vars($data));
@@ -27,18 +35,19 @@
   $totalDeaths = 0;
   $totalConfirmed = 0;
 
+  // Loop para processar os dados recebidos da API
   for ($counter = 0; $counter < $numberKeys; $counter++) {
     array_push($countries, $data->{$counter});
     $totalDeaths = $totalDeaths + $data->{$counter}->Mortos;
     $totalConfirmed = $totalConfirmed + $data->{$counter}->Confirmados;
   };
 
+  // Inclui o arquivo para inserção dos dados no banco de dados
   require './database/insert.php';
 
   ?>
 
-
-
+  <!-- Exibição dos dados do país selecionado -->
   <session>
     <h3 class="country-name"><?php echo $country ?></h3>
     <p class="confirmed-cases-number"><?php echo number_format($totalConfirmed, 0, ',', '.') ?></p>
@@ -47,22 +56,29 @@
     <p class="confirmed-label deaths">ÓBITOS CONFIRMADOS</p>
   </session>
 
-
-
-
-
-
   <!--
   <session>
-    <?php foreach ($countries as $valor) { ?>
-      <h5><?php echo 'Estado: ', $valor->ProvinciaEstado ?></h5>
-      <p><h6><?php echo 'Casos confirmados: ', $valor->Confirmados ?></h6></p>
-      <p><h6><?php echo 'Óbitos: ', $valor->Mortos ?></h6></p>
-    <?php } ?>
+    <?php
+      // Loop para iterar sobre os dados de cada estado do país selecionado
+     foreach ($countries as $valor) { 
+    ?>
+      <?php 
+      echo 'Estado: ', $valor->ProvinciaEstado 
+      ?>
+      <?php 
+      echo 'Casos confirmados: ', $valor->Confirmados 
+      ?>
+
+      <?php 
+      echo 'Óbitos: ', $valor->Mortos 
+      ?>
+    <?php 
+    } ?>
   </session>
   -->
 
   <script>
+    // Processamento dos dados para o gráfico
     var statesTotal = "<?php
                         foreach ($countries as $valor) {
                           echo $valor->ProvinciaEstado, "-";
@@ -90,11 +106,11 @@
     let death = deathString.map(Number);
   </script>
 
-  <script src="js/chart.min.js">
-  </script>
+  <!-- Inclusão do script do Chart.js -->
+  <script src="javascript/chart.min.js"></script>
 
-  <script src="js/chartData.js">
-  </script>
+  <!-- Inclusão do script com os dados para o gráfico -->
+  <script src="javascript/chartData.js"></script>
 
 </body>
 
